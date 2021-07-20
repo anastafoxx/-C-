673542c66b87b385206fe4f57e9b2b94debcf26b
anastafoxx <72817240+anastafoxx@users.noreply.github.com>
2021-07-20 23:50:26 +0300

    Часть приложения на WPF с БД в SQLite
    Текстовый редактор и авторизация/удаление пользователя из БД

---
+++ AppContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace Window25
+{
+    class AppContext : DbContext
+    { public DbSet<User> Users { get; set; }
+        public AppContext() : base("DefaultConnection")
+        {
+
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Window25
+{
+    class User
+    {
+        public int id { get; set; }
+     /*   public int Id
+        {
+            get;set;
+        } */
+
+        private string login, password;
+
+        public string Login   //аксессор точно такой же, но с большой буквы
+        {
+            get { return login; }
+            set { login = value; }
+        }
+        public string Password   //аксессор точно такой же, но с большой буквы
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public User()
+        {
+        }
+
+        public User(string login, string password)
+        {
+            Login = login;
+            Password = password;
+        }
+
+        /*public override string ToString()
+        {
+            return "login is " + login;
+        }*/
+    }
+}
